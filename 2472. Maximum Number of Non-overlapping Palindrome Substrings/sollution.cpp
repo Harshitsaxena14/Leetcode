f415e788b118cfbee7a6af721a6c4fdd7b2a84a9
Harshit Saxena <harshitacademics@gmail.com>
2026-09-16 01:17:31 +0530

    Add palindrome substring solution

---
+++ 2472. Maximum Number of Non-overlapping Palindrome Substrings/sollution.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int maxPalindromes(string s, int k)
+    {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        for (int i = 0; i < n; i++)
+        {
+            dp[i + 1] = max(dp[i + 1], dp[i]);
+            // Odd length palindrome
+            for (int l = i, r = i; l >= 0 && r < n; l--, r++)
+            {
+                if (s[l] != s[r])
+                    break;
+                if (r - l + 1 >= k)
+                {
+                    dp[r + 1] = max(dp[r + 1], dp[l] + 1);
+                }
+            }
+            // Even length palindrome
+            for (int l = i, r = i + 1; l >= 0 && r < n; l--, r++)
+            {
+                if (s[l] != s[r])
+                    break;
+                if (r - l + 1 >= k)
+                {
+                    dp[r + 1] = max(dp[r + 1], dp[l] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+};
